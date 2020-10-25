a3c2c4047869ad35253f944c0fe4cca2ecbb4baa
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-10-25 18:01:59 +0300

    handle unsupported routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
+const HttpError = require('./models/http-error')
 
 //express app
 const app = express()
@@ -22,6 +23,12 @@ app.listen(port, () => console.log(`server is running on port ${port}`))
 //Routes
 app.use("/api/places",placesRoutes)
 app.use("/api/users",usersRoutes)
+
+//unspportes route handling like (http://localhost:5000/api/u2)
+app.use((req,res,next)=>{
+  const error = new HttpError("could not find this route." , 404)
+  return next(error)
+})
 
 //if we have any error in above middlware the below middleware function run
 //ERROR HANDLING MIDLLEWARE FUNCTION
