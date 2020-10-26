45d2d18881f58b6d4ed140cd79bad0a3bd989ba7
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-10-26 13:47:11 +0300

    finish user & place routes

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,3 +1,6 @@
+const {v4} = require('uuid')
+const HttpError = require('../models/http-error')
+
 const DUMMY_USERS = [
   {
     id: 'u1',
@@ -10,8 +13,39 @@ const DUMMY_USERS = [
 const getUsers = (req, res, next) => {
   res.json({users: DUMMY_USERS})
 }
-const signup = (req, res, next) => {}
-const login = (req, res, next) => {}
+
+const signup = (req, res, next) => {
+  const {name, password, email} = req.body
+
+  const hasUser = DUMMY_USERS.find(user => user.email === email)
+
+  if (hasUser) {
+    //422 user input error
+    throw new HttpError('Could not create user, email already exist', 422)
+  }
+
+  const createdUser = {
+    id: v4(),
+    name,
+    email,
+    password,
+  }
+  DUMMY_USERS.unshift(createdUser)
+  res.status(201).json({user: createdUser})
+}
+
+const login = (req, res, next) => {
+  const {email, password} = req.body
+  const identifiedUser = DUMMY_USERS.find(user => user.email === email)
+
+  //
+  if (!identifiedUser || identifiedUser.password !== password) {
+    //401 authentication error
+    throw new HttpError('Could not find the user, credential seems to be wrong', 401)
+  }
+
+  res.json({message: 'logged in'})
+}
 
 exports.getUsers = getUsers
 exports.signup = signup
